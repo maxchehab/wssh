91bdf8f6e94d06b32a66262b4dea88d4fa6a6317
maxchehab <maxchehab@gmail.com>
2017-11-14 18:12:24 -0800

    Implement tab concatenation

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import {
   Tab,
   LinearProgress,
   AppBar,
-  Toolbar
+  Toolbar,
+  FontIcon
 } from "../components/material-ui/index";
 import MuiThemeProvider from "../components/material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "../components/material-ui/styles/getMuiTheme";
@@ -24,35 +25,73 @@ const theme = getMuiTheme({
 export default class extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: 0,
+      tabs: [
+        <Tab key={0} value={0} style={{ width: "10%" }} label="Terminal">
+          <Terminal />
+        </Tab>
+      ]
+    };
   }
+
+  newTerminal() {
+    let tabs = this.state.tabs.slice();
+    tabs.push(
+      <Tab
+        key={this.state.tabs.length}
+        value={this.state.tabs.length}
+        style={{ width: "10%" }}
+        label="Terminal"
+      >
+        <Terminal />
+      </Tab>
+    );
+    this.setState({
+      tabs: tabs,
+      value: this.state.tabs.length
+    });
+  }
+
+  handleChange = value => {
+    this.setState({
+      value: value
+    });
+    if (value == "new") {
+      this.newTerminal();
+    }
+  };
 
   render() {
     return (
       <div>
         <style global jsx>{`
-          [data-reactroot] {
-            height: 100% !important;
-          }
-
           body {
             margin: 0;
           }
         `}</style>
         <link
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons"
           rel="stylesheet"
         />
         <MuiThemeProvider muiTheme={theme}>
-          <Tabs inkBarStyle={{ width: "10%" }}>
-            <Tab style={{ width: "10%" }} label="Terminal">
-              <Terminal />
-            </Tab>
-            <Tab style={{ width: "10%" }} label="Terminal">
-              <Terminal />
-            </Tab>
-            <Tab style={{ width: "10%" }} label="Terminal">
-              <Terminal />
-            </Tab>
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            inkBarStyle={{ width: "10%" }}
+          >
+            {this.state.tabs}
+            <Tab
+              key={1}
+              value={"new"}
+              style={{
+                width: "48px",
+                position: "absolute",
+                right: 0,
+                boxShadow: "rgb(0, 0, 0) 0px 0px 3px"
+              }}
+              icon={<i className="material-icons">add_to_queue</i>}
+            />
           </Tabs>
         </MuiThemeProvider>
       </div>
