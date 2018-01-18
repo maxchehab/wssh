83b334c32f4b4c0ad240358efceabc4efc0b009b
maxchehab <maxchehab@gmail.com>
2018-01-18 14:28:22 -0800

    Styled markdown

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
 import AppBar from "material-ui/AppBar";
 import Button from "material-ui/Button";
-import Markdown from "react-remarkable";
+import ReactMarkdown from "react-markdown";
 import Tabs, { Tab } from "material-ui/Tabs";
 import Terminal from "../components/Terminal";
 import TerminalList from "../components/TerminalList";
+import MarkdownStyle from "../components/MarkdownStyle";
 
 let tabCount = 0;
 let deleting = false;
@@ -168,6 +169,7 @@ class ScrollableTabsButtonAuto extends React.Component {
             opacity: 1 !important;
           }
         `}</style>
+        <MarkdownStyle />
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons"
           rel="stylesheet"
@@ -184,46 +186,48 @@ class ScrollableTabsButtonAuto extends React.Component {
             {this.state.tabs}
           </Tabs>
         </AppBar>
-        <div>
+        <div style={{ display: "flex" }}>
           <div style={{
-            width: this.state.helping ? "75%" : "100%",
+            width: this.state.helping ? "60%" : "100%",
             float: "left"
           }}>
             <TerminalList
-
               currentValue={this.state.value}
               terminals={this.state.terminals}
             />
           </div>
-          <div style={{
-            width: this.state.helping ? "25%" : "0",
+          <div className="markdown-body" style={{
+            width: this.state.helping ? "40%" : "0",
             height: this.state.height,
             display: this.state.helping ? "block" : "none",
             float: "right",
-            overflowY: "auto"
+            overflowY: "auto",
+            paddingLeft: 16,
+            paddingRight: 16
           }}>
-            <Markdown>{this.state.cheatsheet}</Markdown>
+            <ReactMarkdown source={this.state.cheatsheet} />
           </div>
         </div>
-        {this.state.tabs.length < 10 && (
-          <Button
-            className="new-terminal"
-            onClick={() => this.newTerminal()}
-            fab
-            style={{
-              position: "fixed",
-              bottom: 18,
-              right: 18,
-              opacity: 0.5
+        {
+          this.state.tabs.length < 10 && (
+            <Button
+              className="new-terminal"
+              onClick={() => this.newTerminal()}
+              fab
+              style={{
+                position: "fixed",
+                bottom: 18,
+                right: 18,
+                opacity: 0.5
 
-            }}
-            color="primary"
-            aria-label="add"
-          >
-            <i className="material-icons">add_to_queue</i>
-          </Button>
-
-        )}
+              }}
+              color="primary"
+              aria-label="add"
+            >
+              <i className="material-icons">add_to_queue</i>
+            </Button>
+          )
+        }
         <Button
           className="new-terminal"
           onClick={() => { this.setState({ helping: !this.state.helping }) }}
@@ -240,7 +244,7 @@ class ScrollableTabsButtonAuto extends React.Component {
         >
           <i className="material-icons">{this.state.helping ? "close" : "help"} </i>
         </Button>
-      </div>
+      </div >
     );
   }
 }
