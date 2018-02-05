8b4a093bef6e6e54262d39f368cfe2d9ebac4e75
maxchehab <maxchehab@gmail.com>
2018-02-05 09:42:12 -0800

    Implemented username and password caching

--- docker/wssh/ssh.pl
+++ docker/wssh/ssh.pl
@@ -1,24 +1,42 @@
 #!/usr/bin/perl
-my $init;
-open(my $fh, '<', "init.sh") or die "cannot open file"; {
-    local $/;
-    $init = <$fh>;
-}
-close($fh);
+no warnings;
+use feature 'say';
+use Term::ReadKey;
 
-print "Enter your username: ";
-my $username = <STDIN>;
+my $init;
+open(my $ifh, '<', "init.sh") or die "cannot open file"; {
+    local $/;
+    $init = <$ifh>;
+}
+close($ifh);
+
+my $username;
+open(my $ufh, '<', "username") or $username = ""; {
+    local $/;
+    $username = <$ufh>;
+}
+close($ufh);
+
+$username = `bash -c "read -p 'Enter your username: ' -i '$username' -e 'USERNAME' && echo \\\$USERNAME"`;
 chomp ( $username );
 $username =~ s/[^a-zA-Z0-9]//g;
+
 if ($username eq ""){
     print "Invalid username.\n";
 }else {
+    open(my $ufh, '>', "username") or die "cannot open file";
+    print $ufh "$username";
+    close $ufh;
+
     print "$username\@ada.gonzaga.edu's password: ";
-    use Term::ReadKey;
     ReadMode('noecho');
     my $password = ReadLine(0);
     ReadMode('normal');
+
+    open(my $pfh, '>', "password") or die "cannot open file";
+    print $pfh "$password";
+    close $pfh;
+
     print "\n";
-    my $status = system ("sshpass -p '$password' ssh -t $username\@ada.gonzaga.edu '$init'");
-    exec("echo $status");
+    exec("sshpass -p '$password' ssh -t $username\@ada.gonzaga.edu '$init'");
 }
